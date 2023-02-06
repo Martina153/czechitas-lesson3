d50cc3d3006237d3c7a05ec558a6a67c5ae12156
Martina_Harsanikova <martinaharsanikova@gmail.com>
2023-02-06 17:49:36 +0100

    new lines

---
+++ ukol.js
@@ -0,0 +1,7 @@
+// tady je místo pro náš program
+
+let jmeno = prompt("Zadej svoje jmeno")
+let vek = Number(prompt("Zadej svoj vek"))
+
+document.body.innerHTML += "<p>" + celeJmeno + ", vek: " + vek + "</p>";
+
